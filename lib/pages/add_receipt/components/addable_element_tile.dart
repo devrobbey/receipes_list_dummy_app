e01fb06bbca69799a99dcfd68f72b0574fab6490
Robert Palm <devrobbey@gmail.com>
2021-10-10 20:28:29 +0200

    firebase app registration and config insertion

---
+++ lib/pages/add_receipt/components/addable_element_tile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddableElementTile extends StatefulWidget {
+  const AddableElementTile({Key? key}) : super(key: key);
+
+  @override
+  State<AddableElementTile> createState() => _AddableElementTileState();
+}
+
+class _AddableElementTileState extends State<AddableElementTile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
